34a18e04b178fed4f4db720d8942bacd9b4a4d66
VighneshBilgi <vhbilgi@gmail.com>
2022-08-07 12:19:05 +0530

    Added: UC3_Enqueue

--- src/com/bridgelabz/stackandqueue/LinkedList.java
+++ src/com/bridgelabz/stackandqueue/LinkedList.java
@@ -1,8 +1,21 @@
 package com.bridgelabz.stackandqueue;
 
 public class LinkedList<E> {
-
     Node<E> top;
+    Node<E> front;
+    Node<E> rear;
+
+    public void enqueue(E key) {
+        Node<E> newNode=new Node<>(key);
+        if(rear==null){
+            front=newNode;
+            rear=newNode;
+        }
+        else {
+            rear.next = newNode;
+            rear = newNode;
+        }
+    }
 
     public void push(E key){
         Node<E> newNode = new Node<>(key);
@@ -20,8 +33,25 @@ public class LinkedList<E> {
 
     }
 
+    public void displayQueue(){
 
-    public void display(){
+        Node<E> temp = front;
+
+        if(front==null){
+            System.out.println("Queue is empty");
+        }
+
+        while (temp != null) {
+            System.out.print(temp.key);
+            temp = temp.next;
+            if (temp != null) {
+                System.out.print("->");
+            }
+        }
+        System.out.println();
+    }
+
+    public void displayStack(){
 
         Node<E> temp = top;
 
--- src/com/bridgelabz/stackandqueue/StackAndQueue.java
+++ src/com/bridgelabz/stackandqueue/StackAndQueue.java
@@ -10,7 +10,7 @@ public class StackAndQueue {
         linkedList.push(30);
         linkedList.push(56);
         System.out.println("Printing elements of Stack : ");
-        linkedList.display();
+        linkedList.displayStack();
 
         System.out.println("UC2: Peeking and Popping elements of Stack : ");
         System.out.println("Peeking top of stack : ");
@@ -18,20 +18,27 @@ public class StackAndQueue {
         System.out.println("Popping top of stack... ");
         linkedList.pop();
         System.out.println("Printing elements of Stack : ");
-        linkedList.display();
+        linkedList.displayStack();
         System.out.println("Peeking top of stack : ");
         linkedList.peek();
         System.out.println("Popping top of stack... ");
         linkedList.pop();
         System.out.println("Printing elements of Stack : ");
-        linkedList.display();
+        linkedList.displayStack();
         System.out.println("Peeking top of stack : ");
         linkedList.peek();
         System.out.println("Popping top of stack... ");
         linkedList.pop();
         System.out.println("Printing elements of Stack : ");
-        linkedList.display();
+        linkedList.displayStack();
 
+        System.out.println("UC3: Enqueueing elements 70,30,56 into Queue : ");
+        LinkedList<Integer> linkedList2 = new LinkedList<>();
+        linkedList2.enqueue(56);
+        linkedList2.enqueue(30);
+        linkedList2.enqueue(70);
+        System.out.println("Printing elements of Queue : ");
+        linkedList2.displayQueue();
 
     }
 }
